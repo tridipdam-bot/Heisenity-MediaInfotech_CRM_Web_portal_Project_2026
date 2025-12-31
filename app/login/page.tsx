0439d16285fc76bb365df7478f718d90cbb28135
Sayantan <sensayantan9991@gmail.com>
2025-12-31 09:05:12 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 import LoginPage from "@/components/LoginPage";
 import { useRouter, useSearchParams } from "next/navigation";
